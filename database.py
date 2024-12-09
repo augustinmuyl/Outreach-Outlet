6cdf28ab1cf0c1287c855839bab363eeeccd4df6
Augustin <augustinusmp@gmail.com>
2024-12-09 13:44:35 -0500

    User Implementation

--- database.py
+++ database.py
@@ -1,13 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 
 db = SQLAlchemy()
 
 
-# Association table
+# Association tables
 opportunity_categories = db.Table('opportunity_categories', 
     db.Column('opportunity_id', db.Integer, db.ForeignKey('opportunity.id'), primary_key=True),
     db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True))
+
+
+user_opportunities = db.Table('user_opportunities',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('opportunity_id', db.Integer, db.ForeignKey('opportunity.id'), primary_key=True))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
+    saved_opportunities = db.relationship('Opportunity', secondary=user_opportunities, back_populates='users')
 
 
 class Opportunity(db.Model):
@@ -17,6 +31,7 @@ class Opportunity(db.Model):
     org_logo = db.Column(db.String())
     description = db.Column(db.Text)
     categories = db.relationship('Category', secondary=opportunity_categories, back_populates='opportunities')
+    users = db.relationship('User', secondary=user_opportunities, back_populates='saved_opportunities')
 
 
 class Category(db.Model):
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, flash
 from flask_scss import Scss
-from database import db, Opportunity, Category
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from database import db, Opportunity, Category, User
 from api import Volunteer_API, Data_Processor
 
 
@@ -9,6 +15,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['SECRET_KEY'] = 'secret_key'
     Scss(app)
 
     db.init_app(app)
@@ -20,11 +27,31 @@ def create_app():
 
 
 app = create_app()
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 with app.app_context():
     base_url = 'https://www.volunteerconnector.org/api/search/'
     api = Volunteer_API(base_url)
     data_processor = Data_Processor(api)
     #data_processor.process_data()
+
+
+# User loader and Forms
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=150)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=150)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
 
 
 # Home page
@@ -54,6 +81,116 @@ def results(current_category):
 @app.route('/opportunity/<opportunity_id>', methods=['GET', 'POST'])
 def opportunity(opportunity_id):
     return render_template('opportunity.html', opportunity=data_processor.get_opportunity(opportunity_id))
+
+
+# Registration page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter(
+            (User.username == form.username.data) | (User.email == form.email.data)
+            ).first()
+        if existing_user:
+            if existing_user.username == form.username.data:
+                flash('Username already taken. Please choose another.', 'danger')
+            if existing_user.email == form.email.data:
+                flash('Email already registered. Please use another.', 'danger')
+        else:
+            hashed_password = generate_password_hash(form.password.data, method='pbkdf2:sha256')
+            new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created successfully!', 'success')
+            return redirect('/login')
+    return render_template('register.html', form=form)
+
+
+# Login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash('Login Successful!', 'success')
+            return redirect('/')
+        else:
+            flash('Login Unsuccessful. Check email and password', 'danger')
+    return render_template('login.html', form=form)
+
+
+# Pofile page
+@app.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    return render_template('profile.html', user=current_user)
+
+
+# Logout page
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'info')
+    return redirect('/')
+
+
+# Delete account page
+@app.route('/delete_account', methods=['GET', 'POST'])
+@login_required
+def delete_account():
+    db.session.delete(current_user)
+    db.session.commit()
+    flash('Account deleted successfully.', 'info')
+    return redirect('/')
+
+
+# User opportunities page
+@app.route('/saved_opportunities', methods=['GET', 'POST'])
+@login_required
+def my_opportunities():
+    user_opportunities = current_user.saved_opportunities
+    return render_template('saved_opportunities.html', opportunities=user_opportunities)
+
+
+# Add opportunity page
+@app.route('/save_opportunity/<opportunity_id>', methods=['GET', 'POST'])
+@login_required
+def add_opportunity(opportunity_id):
+    user_opportunities = current_user.saved_opportunities
+    opportunity = Opportunity.query.get_or_404(opportunity_id)
+    if opportunity not in user_opportunities:
+        user_opportunities.append(opportunity)
+        db.session.commit()
+        flash('Opportunity saved successfully!', 'success')
+    else:
+        flash('Opportunity already saved.', 'info')
+    return redirect(f'/opportunity/{opportunity_id}')
+
+
+# Remove opportunity page
+@app.route('/remove_opportunity/<opportunity_id>', methods=['GET', 'POST'])
+@login_required
+def remove_opportunity(opportunity_id):
+    user_opportunities = current_user.saved_opportunities
+    opportunity = Opportunity.query.get_or_404(opportunity_id)
+    if opportunity in user_opportunities:
+        user_opportunities.remove(opportunity)
+        db.session.commit()
+        flash('Opportunity removed successfully!', 'success')
+    else:
+        flash('Opportunity not saved.', 'info')
+    return redirect('/saved_opportunities')
+
+
+# User opportunity page
+@app.route('/user_opportunity/<opportunity_id>', methods=['GET', 'POST'])
+@login_required
+def user_opportunity(opportunity_id):
+    opportunity = Opportunity.query.get_or_404(opportunity_id)
+    return render_template('user_opportunity.html', opportunity=opportunity)
 
 
 if __name__ == '__main__':
