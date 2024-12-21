2fe14b40090318192de4343ccecdd1a46c25e590
Augustin <augustinusmp@gmail.com>
2024-12-21 14:22:39 -0500

    PostgreSQL

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import atexit
 import os
 from dotenv import load_dotenv
+from flask_migrate import Migrate
 
 
 load_dotenv()
@@ -19,16 +20,14 @@ load_dotenv()
 # Initialize Flask app
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     Scss(app)
 
     db.init_app(app)
+    migrate = Migrate(app, db)
 
-    with app.app_context():
-        db.create_all()
-    
     return app
 
 
@@ -49,6 +48,7 @@ def process_data_with_context():
 
 
 # Scheduler
+process_data_with_context()
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=process_data_with_context, trigger='interval', minutes=60)
 scheduler.start()
